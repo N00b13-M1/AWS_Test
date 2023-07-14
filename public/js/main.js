b2a64aefcdb235041a1fd2bb4c6b5fd7bb56a660
N00b13-M1 <80034669+N00b13-M1@users.noreply.github.com>
2023-07-14 06:41:48 +0200

    js_trial_mc

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+
+console.log("test");
+
+var x = 2;
+var x = 4;
+
+console.log(x);
+
+let y =3;
+y=6;
+console.log(y)
+
+const z =4;
+
+// z=6
+
+
+const products = [1,2,3,"A",5];
+console.log(products[3]);
+
+for (let test of products) {
+    console.log(test);
+};
+
+const fruits = ["pear","apple","lemon","grape","strawberry"];
+console.log(fruits[3]);
+
+for (let test in fruits) {
+
+    console.log(fruits[test]);
+};
