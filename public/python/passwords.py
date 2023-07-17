f2fd3305ad2f1f2f75c3424526fd6d40e3b64739
N00b13-M1 <80034669+N00b13-M1@users.noreply.github.com>
2023-07-17 20:22:02 +0200

    amended_links_mcs

--- public/python/passwords.py
+++ public/python/passwords.py
@@ -1,13 +1,19 @@
 import itertools
 
-# Generate all possible combinations of 10-digit numbers
-digits = range(10)
-lijst = itertools.product(digits, repeat=1)
-passwords= list(lijst)
+
+digits = "0123456789"
+combinations = itertools.product(digits, repeat=10)
+
+# Iterate over the combinations
+for combo in combinations:
+    print(''.join(combo))
+
+# Alternatively, convert to a list
+# combinations_list = list(combinations)
+# print(combinations_list)
+
+
+
 # # Save the generated passwords to a file
 # with open("passwords.txt", "w") as file:
 #     file.write('\n'.join(passwords))
-
-
-
-print(passwords)
