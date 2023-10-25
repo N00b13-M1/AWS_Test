d8a2b90b93aa76d5fe439c272e61c22ce23496e9
N00b13-M1 <80034669+N00b13-M1@users.noreply.github.com>
2023-10-25 10:24:43 +0200

    css_add_mc

---
+++ Training_folder/5.0_CSS/public/js/main.js
@@ -0,0 +1,27 @@
+let number = false
+if (number != false) {
+    console.log("The number is positive.");
+} else {
+    console.log("The number is not positive.");
+}
+
+// number = !number
+// console.log(number)
+
+
+// let number = 5
+// // console.log(!number)
+// if (!number) {
+//     console.log(number)
+// } else {
+//     console.log("test")
+// }
+
+// let x;
+
+// console.log(x)
+
+// let x = "test"
+
+
+
