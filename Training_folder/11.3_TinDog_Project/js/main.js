59c5b59dde37a39b2aebb05696ed35a684f62b1c
N00b13-M1 <80034669+N00b13-M1@users.noreply.github.com>
2023-11-23 15:02:45 +0100

    test1_js_mc

---
+++ Training_folder/11.3_TinDog_Project/js/main.js
@@ -0,0 +1,34 @@
+// console.log('test');
+// // window.alert("hallokes");
+// // alert("test"); 
+
+
+// for (i=0; i < 10; i++){
+//     console.log(0);
+    // for (j=1; j < 10; j++) {
+    //     console.log(j);
+    // }
+    // console.log(i+"a");
+
+
+
+// for (i=0; i < 20; i++){
+//     console.log(i++);
+// }
+// let a = 2;
+// let b = 3;
+
+// console.log(a+b);
+
+// let a = 0
+// console.log(a++);
+// console.log(a)
+
+
+
+for (let i=0; i < 3; i++){
+    console.log(i);
+    for (let j=i; j < 2; j++){
+        console.log("a");
+    }
+}
